244d022b0db4a75d03321f385cffc6b8a631599f
koo-lab <koolaboratory.gmail.com>
2021-01-28 08:49:30 -0500

    added convatt

---
+++ convatt_rc.py
@@ -0,0 +1,134 @@
+from tensorflow import keras
+import numpy as np
+import sys
+
+sys.path.append('..')
+from tfomics.layers import MultiHeadAttention, RevCompConv1D, RevCompMaxPool
+
+
+def model(input_shape, num_labels, filters, dims, num_heads=12, num_layers=4, pool_size=5, 
+          num_units=1024, activation='relu', bn=False):
+
+  # l2 regularization
+  l2 = keras.regularizers.l2(1e-6)
+
+  # input layer
+  inputs = keras.layers.Input(shape=input_shape)
+
+  # layer 1 - convolution
+  nn, nn2 = keras.layers.RevCompConv1D(filters=filters, kernel_size=19, use_bias=True, padding='same',
+                                  kernel_regularizer=l2, concat=False)(inputs)        
+  if bn:
+    nn = keras.layers.BatchNormalization()(nn)
+  nn = keras.layers.Activation(activation)(nn)
+  nn = keras.layers.MaxPool1D(pool_size=pool_size)(nn)
+  nn = keras.layers.Dropout(0.1)(nn)
+
+  nn2 = keras.layers.Activation(activation)(nn2)
+  nn2 = keras.layers.MaxPool1D(pool_size=pool_size)(nn2)
+  nn2 = keras.layers.Dropout(0.1)(nn2)
+
+  forward_layer = keras.layers.LSTM(dims//2, return_sequences=True)
+  backward_layer = keras.layers.LSTM(dims//2, activation='relu', return_sequences=True, go_backwards=True)
+  nn = keras.layers.Bidirectional(forward_layer, backward_layer=backward_layer)(nn)
+
+  forward_layer2 = keras.layers.LSTM(dims//2, return_sequences=True)
+  backward_layer2 = keras.layers.LSTM(dims//2, activation='relu', return_sequences=True, go_backwards=True)
+  nn2 = keras.layers.Bidirectional(forward_layer2, backward_layer=backward_layer)(nn2)
+
+  nn = RevCompMaxPool()(nn, nn2)
+  nn = keras.layers.Dropout(0.1)(nn)
+
+  #nn = keras.layers.Add()([nn, nn2])
+  nn = keras.layers.LayerNormalization(epsilon=1e-6)(nn)
+
+  nn2,_ = MultiHeadAttention(d_model=dims, num_heads=num_heads)(nn, nn, nn)
+  nn2 = keras.layers.Dropout(0.1)(nn2)
+  #nn = keras.layers.Add()([nn, nn2])
+  #nn = keras.layers.LayerNormalization(epsilon=1e-6)(nn)
+
+  # layer 3 - Fully-connected 
+  nn = keras.layers.Flatten()(nn)
+  nn = keras.layers.Dense(num_units, activation=None, use_bias=False)(nn)      
+  nn = keras.layers.BatchNormalization()(nn)
+  nn = keras.layers.Activation('relu')(nn)
+  nn = keras.layers.Dropout(0.5)(nn)
+
+  #nn = keras.layers.Dense(num_units, activation=None, use_bias=False)(nn)      
+  #nn = keras.layers.BatchNormalization()(nn)
+  #nn = keras.layers.Activation('relu')(nn)
+  #nn = keras.layers.Dropout(0.5)(nn)
+  
+  # Output layer
+  logits = keras.layers.Dense(num_labels, activation='linear', use_bias=True)(nn)
+  outputs = keras.layers.Activation('sigmoid')(logits)
+
+  # create keras model
+  model = keras.Model(inputs=inputs, outputs=outputs)
+
+  return model
+
+
+
+
+def model(input_shape, num_labels, dims, num_heads=12, num_layers=4, pool_size=5, 
+          num_units=1024, activation='relu', bn=False):
+
+  # l2 regularization
+  l2 = keras.regularizers.l2(1e-6)
+
+  # input layer
+  inputs = keras.layers.Input(shape=input_shape)
+
+  # layer 1 - convolution
+  nn, nn2 = RevCompConv1D(filters=dims, kernel_size=19, use_bias=True, padding='same',
+                           kernel_regularizer=l2, concat=False)(inputs)        
+  if bn:
+    nn = keras.layers.BatchNormalization()(nn)
+    nn2 = keras.layers.BatchNormalization()(nn2)
+  nn = keras.layers.Activation(activation)(nn)
+  nn2 = keras.layers.Activation(activation)(nn2)
+  nn = RevCompMaxPool()(nn, nn2)
+  nn = keras.layers.MaxPool1D(pool_size=pool_size)(nn)
+  nn = keras.layers.Dropout(0.1)(nn)
+
+  forward_layer = keras.layers.LSTM(dims//2, return_sequences=True)
+  backward_layer = keras.layers.LSTM(dims//2, activation='relu', return_sequences=True, go_backwards=True)
+  nn2 = keras.layers.Bidirectional(forward_layer, backward_layer=backward_layer)(nn)
+  nn = keras.layers.Dropout(0.1)(nn2)
+  #nn = keras.layers.Add()([nn, nn2])
+  nn = keras.layers.LayerNormalization(epsilon=1e-6)(nn)
+
+  for i in range(num_layers):
+    nn2,_ = MultiHeadAttention(d_model=dims, num_heads=num_heads)(nn, nn, nn)
+    nn2 = keras.layers.Dropout(0.1)(nn2)
+    nn = keras.layers.Add()([nn, nn2])
+    nn = keras.layers.LayerNormalization(epsilon=1e-6)(nn)
+
+    nn2 = keras.layers.Dense(16, activation='relu')(nn)
+    nn2 = keras.layers.Dense(dims)(nn2)
+    nn2 = keras.layers.Dropout(0.1)(nn2)
+    nn = keras.layers.Add()([nn, nn2])
+    nn = keras.layers.LayerNormalization(epsilon=1e-6)(nn)
+
+  # layer 3 - Fully-connected 
+  nn = keras.layers.Flatten()(nn)
+  nn = keras.layers.Dense(num_units, activation=None, use_bias=False)(nn)      
+  nn = keras.layers.BatchNormalization()(nn)
+  nn = keras.layers.Activation('relu')(nn)
+  nn = keras.layers.Dropout(0.5)(nn)
+
+  #nn = keras.layers.Dense(num_units, activation=None, use_bias=False)(nn)      
+  #nn = keras.layers.BatchNormalization()(nn)
+  #nn = keras.layers.Activation('relu')(nn)
+  #nn = keras.layers.Dropout(0.5)(nn)
+  
+  # Output layer
+  logits = keras.layers.Dense(num_labels, activation='linear', use_bias=True)(nn)
+  outputs = keras.layers.Activation('sigmoid')(logits)
+
+  # create keras model
+  model = keras.Model(inputs=inputs, outputs=outputs)
+
+  return model
+
