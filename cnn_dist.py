deef485333c7a4194253fe6716c144d1df775df3
koo-lab <koolaboratory.gmail.com>
2021-02-09 18:26:14 -0500

    updates

--- cnn_dist.py
+++ cnn_dist.py
@@ -1,8 +1,8 @@
 from tensorflow import keras
 
 def model(input_shape, num_labels, activation='relu', 
-          units=[24, 32, 64, 96], dropout=[0.2, 0.2, 0.2, 0.5], 
-          bn=[True, True, True, True], l2=None):
+          units=[24, 32, 48, 64, 96], dropout=[0.1, 0.2, 0.3, 0.4, 0.5], 
+          bn=[True, True, True, True, True], l2=None):
 
   # l2 regularization
   if l2 is not None:
@@ -31,24 +31,22 @@ def model(input_shape, num_labels, activation='relu',
     nn = keras.layers.BatchNormalization()(nn)
   nn = keras.layers.Activation(activation)(nn)
   nn = keras.layers.Dropout(dropout[0])(nn)
-  nn = keras.layers.MaxPool1D(pool_size=4)(nn)
 
-  # layer 2
   nn = keras.layers.Conv1D(filters=units[1],
-                           kernel_size=5,
+                           kernel_size=7,
                            strides=1,
                            activation=None,
                            use_bias=use_bias[1],
                            padding='same',
                            kernel_regularizer=l2, 
-                           )(nn)
+                           )(inputs)
   if bn[1]:
     nn = keras.layers.BatchNormalization()(nn)
   nn = keras.layers.Activation(activation)(nn)
-  nn = keras.layers.MaxPool1D(pool_size=4)(nn)
   nn = keras.layers.Dropout(dropout[1])(nn)
+  nn = keras.layers.MaxPool1D(pool_size=4)(nn)
 
-  # layer 3
+  # layer 2
   nn = keras.layers.Conv1D(filters=units[2],
                            kernel_size=5,
                            strides=1,
@@ -63,17 +61,32 @@ def model(input_shape, num_labels, activation='relu',
   nn = keras.layers.MaxPool1D(pool_size=4)(nn)
   nn = keras.layers.Dropout(dropout[2])(nn)
 
-  # layer 4 - Fully-connected 
-  nn = keras.layers.Flatten()(nn)
-  nn = keras.layers.Dense(units[3],
-                          activation=None,
-                          use_bias=use_bias[3],
-                          kernel_regularizer=l2, 
-                          )(nn)      
+  # layer 3
+  nn = keras.layers.Conv1D(filters=units[3],
+                           kernel_size=5,
+                           strides=1,
+                           activation=None,
+                           use_bias=use_bias[3],
+                           padding='same',
+                           kernel_regularizer=l2, 
+                           )(nn)
   if bn[3]:
     nn = keras.layers.BatchNormalization()(nn)
   nn = keras.layers.Activation(activation)(nn)
+  nn = keras.layers.MaxPool1D(pool_size=4)(nn)
   nn = keras.layers.Dropout(dropout[3])(nn)
+
+  # layer 4 - Fully-connected 
+  nn = keras.layers.Flatten()(nn)
+  nn = keras.layers.Dense(units[4],
+                          activation=None,
+                          use_bias=use_bias[4],
+                          kernel_regularizer=l2, 
+                          )(nn)      
+  if bn[4]:
+    nn = keras.layers.BatchNormalization()(nn)
+  nn = keras.layers.Activation(activation)(nn)
+  nn = keras.layers.Dropout(dropout[4])(nn)
 
   # Output layer 
   logits = keras.layers.Dense(num_labels, activation='linear', use_bias=True)(nn)
